13ac3baf73089abeb4ca8ac3754a399b1bbb0e84
Manuel Zubieta <mzubieta@ushuaiavision.com.ar>
2018-03-26 16:42:03 -0300

    Corregido en el filter el nombre del campo

--- web/cron_tasks.py
+++ web/cron_tasks.py
@@ -92,7 +92,7 @@ def update_pendientes():
 
 
 def cargar_nodo():
-    clientes = Cliente.objects.filter(nodo__isnull=True)
+    clientes = Cliente.objects.filter(nodo_fk__isnull=True)
     cont = 0
     con = sigabdConnector(USER, PASS)
     for c in clientes:
