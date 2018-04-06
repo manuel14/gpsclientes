6a98d7a049961cb878ebec359175230e12340b94
Manuel Zubieta <mzubieta@ushuaiavision.com.ar>
2018-04-06 15:54:50 -0300

    Agregado metodo para cargar clientes con tiras

--- web/helpers.py
+++ web/helpers.py
@@ -2,7 +2,12 @@ from openpyxl import load_workbook
 from .models import Cliente, Calle
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from sigabd import sigabdConnector
+from django.http import JsonResponse
+from datetime import datetime
+import json
+import logging
 
+logger = logging.getLogger(__name__)
 
 def load_clientes():
 
@@ -90,6 +95,7 @@ def load_calles():
         c.save()
     return True
 
+
 def update_estado():
     clientes = Cliente.objects.filter(estado__isnull=True)
     for c in clientes:
@@ -99,3 +105,132 @@ def update_estado():
         c.save()
     return True
 
+
+def compare_to_gis():
+    with open('ubicados.geojson') as infile:
+        f = json.load(infile)
+    cont = 0
+    for feature in f["features"]:
+        cliente = Cliente.objects.filter(
+            clientenro=feature["properties"]["clientenro"],
+            latitud_4326__isnull=True
+        )
+        try:
+            cli = cliente[0]
+            cli.latitud_221712 = feature["geometry"]["coordinates"][0]
+            cli.longitud_221712 = feature["geometry"]["coordinates"][1]
+            cli.save()
+            cont += 1
+        except IndexError:
+            continue
+    return JsonResponse(json.dumps(cont))
+
+
+def load_tiras_coords():
+    wb = load_workbook('barrios_ushuaia.xlsx')
+    ws = wb.get_sheet_by_name('tiras')
+    """640viv
+    """
+    r = 1
+    calleidsiga = ws['E%s' % (r)].value
+    cant = 0
+    for r in range(2, 49):
+        clientes = Cliente.objects.filter(
+            latitud_4326__isnull=True,
+            calle__calleidsiga=calleidsiga,
+            tira=ws['B%s' % (r)].value
+        )
+        for c in clientes:
+            c.latitud_4326 = ws['C%s' % (r)].value
+            c.longitud_4326 = ws['D%s' % (r)].value
+            c.precision = 0
+            c.fecha_posicion = datetime.now()
+            c.save()
+            cant += 1
+    """64 viviendas
+    """
+    r = 49
+    calleidsiga = ws['E%s' % (r)].value
+    for r in range(50, 54):
+        clientes = Cliente.objects.filter(
+            latitud_4326__isnull=True,
+            calle__calleidsiga=calleidsiga,
+            tira=ws['B%s' % (r)].value
+        )
+        for c in clientes:
+            c.latitud_4326 = ws['C%s' % (r)].value
+            c.longitud_4326 = ws['D%s' % (r)].value
+            c.precision = 0
+            c.fecha_posicion = datetime.now()
+            c.save()
+            cant += 1
+    """monte gallinero
+
+    """ 
+    r = 54
+    calleidsiga = ws['E%s' % (r)].value
+    for r in range(55, 96):
+        clientes = Cliente.objects.filter(
+            latitud_4326__isnull=True,
+            calle__calleidsiga=calleidsiga,
+            tira=ws['B%s' % (r)].value
+        )
+        for c in clientes:
+            c.latitud_4326 = ws['C%s' % (r)].value
+            c.longitud_4326 = ws['D%s' % (r)].value
+            c.precision = 0
+            c.fecha_posicion = datetime.now()
+            c.save()
+            cant += 1
+    """30 viviendas
+    """
+    r = 96
+    calleidsiga = ws['E%s' % (r)].value
+    for r in range(97, 107):
+        clientes = Cliente.objects.filter(
+            latitud_4326__isnull=True,
+            calle__calleidsiga=calleidsiga,
+            tira=ws['B%s' % (r)].value
+        )
+        for c in clientes:
+            c.latitud_4326 = ws['C%s' % (r)].value
+            c.longitud_4326 = ws['D%s' % (r)].value
+            c.precision = 0
+            c.fecha_posicion = datetime.now()
+            c.save()
+            cant += 1
+    """245 VIVIENDAS
+    """
+    r = 107
+    calleidsiga = ws['E%s' % (r)].value
+    for r in range(108, 139):
+        clientes = Cliente.objects.filter(
+            latitud_4326__isnull=True,
+            calle__calleidsiga=calleidsiga,
+            tira=ws['B%s' % (r)].value
+        )
+        for c in clientes:
+            c.latitud_4326 = ws['C%s' % (r)].value
+            c.longitud_4326 = ws['D%s' % (r)].value
+            c.precision = 0
+            c.fecha_posicion = datetime.now()
+            c.save()
+            cant += 1
+    """60 massiotra
+    """
+    r = 139
+    calleidsiga = ws['E%s' % (r)].value
+    for r in range(140, 160):
+        clientes = Cliente.objects.filter(
+            latitud_4326__isnull=True,
+            calle__calleidsiga=calleidsiga,
+            tira=ws['B%s' % (r)].value
+        )
+        for c in clientes:
+            c.latitud_4326 = ws['C%s' % (r)].value
+            c.longitud_4326 = ws['D%s' % (r)].value
+            c.precision = 0
+            c.fecha_posicion = datetime.now()
+            c.save()
+            cant += 1
+    return JsonResponse({"cantidad": cant})
